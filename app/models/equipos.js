a3f510e9b53e0cda37a360431e4bfb36bdec6ad4
Hector <gt12930@GMAIL.com>
2025-02-19 22:07:59 -0500

    pkg

--- app/models/equipos.js
+++ app/models/equipos.js
@@ -1,91 +1,91 @@
 
 module.exports = (sequelize, DataTypes) =>{
 
-    const equipos = sequelize.define("equipo", {
+  const equipos = sequelize.define("equipo", {
 
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-          },
-        tipo: DataTypes.STRING,
-        marca: DataTypes.STRING,
-        sbn: {
-          type: DataTypes.STRING,
+      id: {
           allowNull: false,
-          unique: true, // Definir 'sbn' como único
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
         },
-        sbn_cpu: DataTypes.STRING,
-        fecha_ingreso: DataTypes.STRING,
-        proveedor: DataTypes.STRING,
-        procesador: DataTypes.STRING,
-        capacidad_disco_duro: DataTypes.STRING,
-        memoria_ram: DataTypes.STRING,
-        tarjeta_video: DataTypes.STRING,
-        estado: DataTypes.STRING,
-        descripcion: DataTypes.STRING,
-        trabajador_id: DataTypes.INTEGER,
-        secuencia: DataTypes.INTEGER,
-        modelo: DataTypes.STRING,
-        caracteristicas: DataTypes.STRING,
-        estado_conserv: DataTypes.STRING,
-        nro_orden: DataTypes.STRING,
-        fecha_compra: DataTypes.DATE,
-        valor_compra: DataTypes.FLOAT,
-        nro_serie: DataTypes.STRING,
-        codigo_barra: DataTypes.STRING,
-        empleado_final: DataTypes.STRING,
-        // Campos que faltan cpu
+      tipo: DataTypes.STRING,
+      marca: DataTypes.STRING,
+      sbn: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Definir 'sbn' como único
+      },
+      sbn_cpu: DataTypes.STRING,
+      fecha_ingreso: DataTypes.STRING,
+      proveedor: DataTypes.STRING,
+      procesador: DataTypes.STRING,
+      capacidad_disco_duro: DataTypes.STRING,
+      memoria_ram: DataTypes.STRING,
+      tarjeta_video: DataTypes.STRING,
+      estado: DataTypes.STRING,
+      descripcion: DataTypes.STRING,
+      trabajador_id: DataTypes.INTEGER,
+      secuencia: DataTypes.INTEGER,
+      modelo: DataTypes.STRING,
+      caracteristicas: DataTypes.STRING,
+      estado_conserv: DataTypes.STRING,
+      nro_orden: DataTypes.STRING,
+      fecha_compra: DataTypes.DATE,
+      valor_compra: DataTypes.FLOAT,
+      nro_serie: DataTypes.STRING,
+      codigo_barra: DataTypes.STRING,
+      empleado_final: DataTypes.STRING,
+      // Campos que faltan cpu
 
-        usuario_actual: DataTypes.STRING,
-        nombre_pc: DataTypes.STRING,
-        tipo_disco_duro: DataTypes.STRING,
-        almacenamiento: DataTypes.STRING,
-        unidad_optica: DataTypes.BOOLEAN,
-        antivirus: DataTypes.BOOLEAN,
-        windows: DataTypes.BOOLEAN,
+      usuario_actual: DataTypes.STRING,
+      nombre_pc: DataTypes.STRING,
+      tipo_disco_duro: DataTypes.STRING,
+      almacenamiento: DataTypes.STRING,
+      unidad_optica: DataTypes.BOOLEAN,
+      antivirus: DataTypes.BOOLEAN,
+      windows: DataTypes.BOOLEAN,
 
-        version_windows: DataTypes.STRING, // licenciado o crackeado
-        sistema_operativo: DataTypes.STRING,
-        ofimatica: DataTypes.STRING, // licenciado o crackeado
-        office: DataTypes.STRING, //version 
-        mac: DataTypes.STRING, 
-        generacion_procesador: DataTypes.STRING,// nuevoooooo
-        nombre_servidor: DataTypes.STRING, // nuevoooooo
+      version_windows: DataTypes.STRING, // licenciado o crackeado
+      sistema_operativo: DataTypes.STRING,
+      ofimatica: DataTypes.STRING, // licenciado o crackeado
+      office: DataTypes.STRING, //version 
+      mac: DataTypes.STRING, 
+      generacion_procesador: DataTypes.STRING,// nuevoooooo
+      nombre_servidor: DataTypes.STRING, // nuevoooooo
 
-        // campos que faltan impresoras
+      // campos que faltan impresoras
 
-        suministro: DataTypes.STRING,
-        imagen: DataTypes.STRING,
-        tamaño: DataTypes.STRING,
-        ip: DataTypes.STRING,
-        subtipo_impresora: DataTypes.STRING,// nuevoooooo
+      suministro: DataTypes.STRING,
+      imagen: DataTypes.STRING,
+      tamaño: DataTypes.STRING,
+      ip: DataTypes.STRING,
+      subtipo_impresora: DataTypes.STRING,// nuevoooooo
 
-        // Monitores
+      // Monitores
 
-        tecnologia_monitor: DataTypes.STRING, // lcd, led
-        pulgadas: DataTypes.STRING,
-        
-        //telefonos
-        anexo: DataTypes.STRING,
-        sede_id: DataTypes.INTEGER,
-        modulo_id: DataTypes.INTEGER,
-        dependencia_id: DataTypes.INTEGER,
-        sub_dependencia_id: DataTypes.INTEGER,
-        dependencia_id: DataTypes.INTEGER,
-        sub_dependencia_id: DataTypes.INTEGER, 
-        createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE
-    }, {timestamps: true, tableName: "equipo", freezeTableName: true})
-    equipos.associate = function(models) {
-        equipos.belongsTo(models.trabajador, { foreignKey: "trabajador_id" });
-        equipos.belongsTo(models.dependencias, {foreignKey: "dependencia_id"})
-        equipos.belongsTo(models.sedes, {foreignKey: "sede_id"})
-        equipos.belongsTo(models.modulos, {foreignKey:"modulo_id"})
-        equipos.belongsTo(models.sub_dependencias, {foreignKey: "sub_dependencia_id"})
-      };
+      tecnologia_monitor: DataTypes.STRING, // lcd, led
+      pulgadas: DataTypes.STRING,
+      
+      //telefonos
+      anexo: DataTypes.STRING,
+      sede_id: DataTypes.INTEGER,
+      modulo_id: DataTypes.INTEGER,
+      dependencia_id: DataTypes.INTEGER,
+      sub_dependencia_id: DataTypes.INTEGER,
+      dependencia_id: DataTypes.INTEGER,
+      sub_dependencia_id: DataTypes.INTEGER, 
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE
+  }, {timestamps: true, tableName: "equipo", freezeTableName: true})
+  equipos.associate = function(models) {
+      equipos.belongsTo(models.trabajador, { foreignKey: "trabajador_id" });
+      equipos.belongsTo(models.dependencias, {foreignKey: "dependencia_id"})
+      equipos.belongsTo(models.sedes, {foreignKey: "sede_id"})
+      equipos.belongsTo(models.modulos, {foreignKey:"modulo_id"})
+      equipos.belongsTo(models.sub_dependencias, {foreignKey: "sub_dependencia_id"})
+    };
 
-    return equipos;
+  return equipos;
 
 } 
--- app/models/index.js
+++ app/models/index.js
@@ -1,25 +1,19 @@
 // app/models/index.js
-
 const Sequelize = require("sequelize");
 const config = require("../../config/database.js");
 const sequelize = new Sequelize(config);
 
 const db = {};
 
-// Importa los modelos de forma estática
-// Asegúrate de que el nombre asignado coincida con el que usarás en tus controladores.
-db.equipo = require("./equipos")(sequelize, Sequelize.DataTypes);
+// Importa los modelos de forma estática y usa nombres consistentes
+db.trabajador = require("./trabajador")(sequelize, Sequelize.DataTypes);
 db.dependencias = require("./dependencias")(sequelize, Sequelize.DataTypes);
-db.modulos = require("./modulos")(sequelize, Sequelize.DataTypes);
-db.sedes = require("./sedes")(sequelize, Sequelize.DataTypes);
+db.sedes      = require("./sedes")(sequelize, Sequelize.DataTypes);
+db.modulos    = require("./modulos")(sequelize, Sequelize.DataTypes);
 db.sub_dependencias = require("./sub_dependencias")(sequelize, Sequelize.DataTypes);
-db.trabajadores = require("./trabajadores")(sequelize, Sequelize.DataTypes);
-db.usuario = require("./usuario")(sequelize, Sequelize.DataTypes);
+db.equipo     = require("./equipos")(sequelize, Sequelize.DataTypes);
 
-// Si tienes más modelos, agrégalos aquí, por ejemplo:
-// db.Usuario = require("./usuario")(sequelize, Sequelize.DataTypes);
-
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
