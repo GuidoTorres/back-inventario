286ad3f149e9f98249f407c5f481432aea804d4e
Hector <gt12930@GMAIL.com>
2025-02-19 22:05:39 -0500

    pkg

--- app/models/index.js
+++ app/models/index.js
@@ -1,30 +1,24 @@
 // app/models/index.js
 
-const fs = require("fs");
-const path = require("path");
 const Sequelize = require("sequelize");
 const config = require("../../config/database.js");
-
-const db = {};
 const sequelize = new Sequelize(config);
 
-fs.readdirSync(__dirname)
-  .filter(file => {
-    // Excluir archivos que no sean .js o el propio index.js
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== path.basename(__filename) &&
-      file.slice(-3) === ".js"
-    );
-  })
-  .forEach(file => {
-    const modelPath = path.join(__dirname, file);
-    // Se requiere el modelo y se le pasa la instancia de sequelize y los DataTypes
-    const model = require(modelPath)(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const db = {};
 
-// Configurar asociaciones (si existen)
+// Importa los modelos de forma estática
+// Asegúrate de que el nombre asignado coincida con el que usarás en tus controladores.
+db.equipo = require("./equipos")(sequelize, Sequelize.DataTypes);
+db.dependencias = require("./dependencias")(sequelize, Sequelize.DataTypes);
+db.modulos = require("./modulos")(sequelize, Sequelize.DataTypes);
+db.sedes = require("./sedes")(sequelize, Sequelize.DataTypes);
+db.sub_dependencias = require("./sub_dependencias")(sequelize, Sequelize.DataTypes);
+db.trabajadores = require("./trabajadores")(sequelize, Sequelize.DataTypes);
+db.usuario = require("./usuario")(sequelize, Sequelize.DataTypes);
+
+// Si tienes más modelos, agrégalos aquí, por ejemplo:
+// db.Usuario = require("./usuario")(sequelize, Sequelize.DataTypes);
+
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
