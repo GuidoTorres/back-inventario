12c6d7525902557c89c27ee28039202dc6656803
Hector <gt12930@GMAIL.com>
2025-02-19 22:08:45 -0500

    pkg

--- app/models/index.js
+++ app/models/index.js
@@ -6,12 +6,13 @@ const sequelize = new Sequelize(config);
 const db = {};
 
 // Importa los modelos de forma estática y usa nombres consistentes
-db.trabajador = require("./trabajador")(sequelize, Sequelize.DataTypes);
+db.trabajador = require("./trabajadores")(sequelize, Sequelize.DataTypes);
 db.dependencias = require("./dependencias")(sequelize, Sequelize.DataTypes);
 db.sedes      = require("./sedes")(sequelize, Sequelize.DataTypes);
 db.modulos    = require("./modulos")(sequelize, Sequelize.DataTypes);
 db.sub_dependencias = require("./sub_dependencias")(sequelize, Sequelize.DataTypes);
 db.equipo     = require("./equipos")(sequelize, Sequelize.DataTypes);
+db.usuario    = require("./usuario")(sequelize, Sequelize.DataTypes);
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
